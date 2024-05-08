e029b48018415baf09ae0e45823f3abdd278dbd6
Mitul K M <123618575+switchtwitch12345@users.noreply.github.com>
2024-05-09 01:07:51 +0530

    Update IA2.py

--- IA2.py
+++ IA2.py
@@ -17,14 +17,14 @@ app.config["SESSION_COOKIE_NAME"] = "myCOOKIE_monSTER528"
 @app.route("/", methods=["POST", "GET"])
 def index():
     session["all_items"], session["shopping_items"] = get_db()
-    return render_template("IA2.html", all_items=session["all_items"],
+    return render_template("index.html", all_items=session["all_items"],
                            shopping_items=session["shopping_items"])
 
 @app.route("/add_items", methods=["POST"])
 def add_items():
     session["shopping_items"].append(request.form["select_items"])
     session.modified = True
-    return render_template("IA2.html", all_items=session["all_items"],
+    return render_template("index.html", all_items=session["all_items"],
                            shopping_items=session["shopping_items"])
 
 @app.route("/delete_items", methods=["POST"])
@@ -34,7 +34,7 @@ def delete_items():
         if item in session["shopping_items"]:
             session["shopping_items"].remove(item)
     session.modified = True
-    return render_template("IA2.html", all_items=session["all_items"],
+    return render_template("index.html", all_items=session["all_items"],
                            shopping_items=session["shopping_items"])
 
 @app.route("/update_items", methods=["POST"])
@@ -47,7 +47,7 @@ def update_items():
         session["shopping_items"][index] = new_item
         session.modified = True
 
-    return render_template("IA2.html", all_items=session["all_items"],
+    return render_template("index.html", all_items=session["all_items"],
                            shopping_items=session["shopping_items"])
 
 def get_db():
